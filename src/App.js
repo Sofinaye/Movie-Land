027e2d757d49d9f889666ef1f0cf98400705a410
Sofinaye <abene.fekadu@gmail.com>
2024-01-19 00:09:00 +0200

    Add structure for the search bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from "react";
-
+import { useEffect } from "react";
+import SearchIcon from "./search.svg"
 //788ba869
  const API_URL = 'http://www.omdbapi.com?apikey=788ba869';
 const App = () => {
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+        
+        console.log(data.Search);
+    }
+    useEffect(()=>{
+        searchMovies('All american');
+    }, []);
     return (
-        <h1>App</h1>
+        <div className='app'>
+            <h1>MovieLand</h1>
+            <div className="search">
+                <input 
+                    placeholder="Search for movies"
+                    value="All American"
+                    onChange={() => {}}
+                />
+            </div>
+        </div>
     );
 }
  export default App;
